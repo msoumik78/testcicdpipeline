1485c6356c6ea3138f831eb530c3b543200883d4
msoumik78 <msoumik@Soumiks-MacBook-Pro.local>
2026-02-13 12:11:01 +0530

    small issue in test

--- app/test_main.py
+++ app/test_main.py
@@ -7,7 +7,7 @@ client = TestClient(app)
 def test_read_main():
     response = client.get("/")
     assert response.status_code == 200
-    assert response.json() == {"msg": "Hello World2"}
+    assert response.json() == {"msg": "Hello World"}
 
 def test_read_item():
     response = client.get("/items/1")
